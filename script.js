7305e71495af606e7725387777fa35e0220bbf60
Deepanshuisjod <deepanshudb1@gmail.com.noreply.github.com>
2023-03-30 13:25:15 +0530

    Modified the index.html script.js and style.css file
    Used Many funtions and eventlisteners and queryselector to get the final result

--- script.js
+++ script.js
@@ -2,23 +2,89 @@ function getComputerschoice(){
     let num=3;
     let computerchoice;
     let Random = Math.floor(Math.random()*3);
-    if (Random==0){
-        computerchoice = "Rock";
-    }
-    else if (Random == 1){
-        computerchoice = "Paper";
-    }
-    else{
-        computerchoice = "Scissor"
-    }
-    return "Computer choose" + computerchoice;
+    
+    return Random;
 }
 
 
+function compchoice(cschoice){
+
+    if (cschoice==0){
+        document.getElementById("CompResult").innerHTML = "Computer's choice :- ROCK";
+    }
+    else if (cschoice==1){
+        document.getElementById("CompResult").innerHTML = "Computer's choice :- PAPER";
+    }
+    else if (cschoice==2){
+        document.getElementById("CompResult").innerHTML = "Computer's choice :- SCISSOR";
+    }
+}
+
 let Rock = document.querySelector(".Rock-btn");
+let Paper = document.querySelector(".Paper-btn");
+let Scissor = document.querySelector(".Scissor-btn");
+
 Rock.addEventListener("click",playerselectrock)
 function playerselectrock(){
-    ;
+    cschoice = getComputerschoice();
+    if (cschoice==0){
+        compchoice(cschoice);
+        tied();
+    }
+    else if (cschoice==1){
+        compchoice(cschoice);
+        loose();
+    }
+    else if (cschoice==2){
+        compchoice(cschoice);
+        win();
+    }
 
+}
+
+Paper.addEventListener("click",playerselectpaper)
+function playerselectpaper(){
+    cschoice = getComputerschoice();
+    if (cschoice==0){
+        compchoice(cschoice);
+        win();
+    }
+    else if (cschoice==1){
+        compchoice(cschoice);
+        tied();
+    }
+    else if (cschoice==2){
+        compchoice(cschoice);
+        loose();
+    }
+
+}
+
+Scissor.addEventListener("click",playerselectscissor)
+function playerselectscissor(){
+    cschoice = getComputerschoice();
+    if (cschoice==0){
+        compchoice(cschoice);
+        loose();
+    }
+    else if (cschoice==1){
+        compchoice(cschoice);
+        win();
+    }
+    else if (cschoice==2){
+        compchoice(cschoice);
+        tied();
+    }
+
+}
+
+function tied(){
+    document.getElementById("Result").innerHTML = "You both tied";
+}
+function loose(){
+    document.getElementById("Result").innerHTML = "You loose";
+}
+function win(){
+    document.getElementById("Result").innerHTML = "You Win";
 }
 
